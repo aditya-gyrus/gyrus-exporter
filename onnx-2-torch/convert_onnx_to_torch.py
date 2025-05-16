500c52c329fb213f00b9740fd6e99e1973f705b6
Aditya <aditya@gyrus.ai>
2025-05-16 16:35:35 +0530

    Inital Setup onnx and hf

---
+++ onnx-2-torch/convert_onnx_to_torch.py
@@ -0,0 +1,35 @@
+import torch
+from onnx2torch import convert
+from transformers import AutoTokenizer,AutoModel
+import os
+import shutil
+
+
+def copy_files_excluding_safetensors(src_dir, dst_dir):
+    os.makedirs(dst_dir, exist_ok=True)
+    for root, _, files in os.walk(src_dir):
+        rel_root = os.path.relpath(root, src_dir)
+        target_root = os.path.join(dst_dir, rel_root) if rel_root != "." else dst_dir
+        os.makedirs(target_root, exist_ok=True)
+
+        for file in files:
+            if file.endswith(".onnx") or file.endswith(".onnx_data"):
+                continue
+            src_file = os.path.join(root, file)
+            dst_file = os.path.join(target_root, file)
+            shutil.copy2(src_file, dst_file)
+
+onnx_model_directory = "../models/onnx-models/bert-base-cased-qa-onnx/"
+
+# onnx_model_path = "../models/onnx-models/bert-base-cased-qa-onnx/model.onnx"
+# torch_model = convert(onnx_model_path)
+
+# torch.save(torch_model.state_dict(), "bert-base-cased-qa/pytorch_model.bin")
+
+# tokenizer = AutoTokenizer.from_pretrained("bert-base-cased-qa/")
+# model = AutoModel.from_pretrained("bert-base-cased-qa/")
+
+
+copy_files_excluding_safetensors(onnx_model_directory,"bert-base-cased-qa/")
+
+
---
+++ safetensors-2-torch/converter_hf_to_torch.py
@@ -0,0 +1,45 @@
+import argparse
+import os
+import shutil
+import torch
+from transformers import AutoModel
+
+def copy_files_excluding_safetensors(src_dir, dst_dir):
+    os.makedirs(dst_dir, exist_ok=True)
+    for root, _, files in os.walk(src_dir):
+        rel_root = os.path.relpath(root, src_dir)
+        target_root = os.path.join(dst_dir, rel_root) if rel_root != "." else dst_dir
+        os.makedirs(target_root, exist_ok=True)
+
+        for file in files:
+            if file.endswith(".safetensors"):
+                continue
+            src_file = os.path.join(root, file)
+            dst_file = os.path.join(target_root, file)
+            shutil.copy2(src_file, dst_file)
+
+
+def main():
+    parser = argparse.ArgumentParser(description="Merge HF model shards and save as pytorch_model.bin")
+    parser.add_argument("--input_dir",type=str,required=True,help="Path to the input Hugging Face model directory")
+    parser.add_argument("--output_dir",type=str,required=True,help=" Output Directory to save the pytorch_model.bin file")
+    args = parser.parse_args()
+
+    print(f"Loading model from {args.input_dir} ...")
+    model = AutoModel.from_pretrained(args.input_dir)
+
+    os.makedirs(args.output_dir, exist_ok=True)
+
+    output_path = os.path.join(args.output_dir, "pytorch_model.bin")
+    print(f"Saving merged weights to {output_path} ...")
+    torch.save(model.state_dict(), output_path)
+
+    copy_files_excluding_safetensors(args.input_dir, args.output_dir)
+
+    print("Done.")
+
+
+#python3 converter_test.py --input_dir ../models/hf-models/phi_1_5 --output_dir phi_output
+
+if __name__ == "__main__":
+    main()
